371f20732ccfc2db404c35fd5e30e496b31314af
Ritesh Kumar <rit8503@gmail.com>
2022-04-03 11:34:45 +0530

    .

--- project/contact_us/models.py
+++ project/contact_us/models.py
@@ -10,7 +10,7 @@ class ContactUs(models.Model):
     first_name = models.CharField(max_length=100, default='')
     last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=200, blank=True)
-    contact=models.IntegerField(default='+91')
+    contact=models.IntegerField(default='+91', editable=True)
 
     class Meta:
         db_table = "contact"
